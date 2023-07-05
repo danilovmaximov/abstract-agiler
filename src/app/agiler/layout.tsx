a4b16e47a023c2809b4db18eb1debe985afcaa3e
ifralou <ilya.fralou@icloud.com>
2023-07-05 18:58:30 +0200

    Main page agiler layout added.

--- src/app/agiler/layout.tsx
+++ src/app/agiler/layout.tsx
@@ -1,13 +1,23 @@
 "use client"
 import {SessionProvider} from "next-auth/react";
 import {Children} from "@/global";
+import {Center, ChakraProvider} from "@chakra-ui/react";
 
 export default function DashboardLayout({children}: Children) {
     return (
         <SessionProvider>
-            <main>
-                { children }
-            </main>
+            <ChakraProvider>
+
+                { /* Main layout wrapper */}
+                <Center as="main"
+                        h="100vh"
+                        minH={"500px"}
+                        bg={"black"}
+                >
+                    {children}
+                </Center>
+
+            </ChakraProvider>
         </SessionProvider>
     )
 
--- src/app/agiler/page.tsx
+++ src/app/agiler/page.tsx
@@ -1,8 +1,46 @@
+"use client"
 import {useSession} from "next-auth/react";
+import {
+    Card, CardBody, CardHeader, Grid, Heading, Skeleton, Text, GridItem
+} from "@chakra-ui/react";
+import React from "react";
 
 export default function Dashboard() {
-        const session = useSession()
-        return <div>
-                {JSON.stringify(session)}
-        </div>
+    // User session hook to get data about user session.
+    const {data} = useSession()
+
+    return (
+        <Grid as={"article"}
+              bg={"white"}
+              w={"90%"} h={"90%"}
+              p={"1rem"}
+
+              templateRows={"repeat(12, 1fr)"}
+              templateColumns={"repeat(12, 1fr)"}
+              gap={4}
+              borderRadius={"5px"}
+        >
+            <GridItem rowSpan={2} colSpan={7}>
+
+                <Skeleton
+                    isLoaded={data?.user != null}
+                    w={"100%"} h={"100%"}
+                >
+                    <Card w={"100%"} h={"100%"}>
+                        <CardHeader>
+                            <Heading>Hi there, {data?.user?.email}</Heading>
+                        </CardHeader>
+                        <CardBody>
+                            <Text>
+                                what is going on?
+                            </Text>
+                        </CardBody>
+                    </Card>
+                </Skeleton>
+
+            </GridItem>
+
+
+        </Grid>
+    )
 }
