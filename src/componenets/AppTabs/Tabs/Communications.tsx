09e83f7f88d3d2ea5a5126b1be9a12892353dbb3
Ilya -  Elijah - Eliáš <109349277+ifralou@users.noreply.github.com>
2023-07-11 22:59:51 +0200

    Tab componenets. (#10)

---
+++ src/componenets/AppTabs/Tabs/Communications.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Heading} from "@chakra-ui/react";
+
+const Communications = () => {
+    return (
+        <div>
+            <Heading>Communication</Heading>
+        </div>
+    );
+};
+
+export default Communications;
